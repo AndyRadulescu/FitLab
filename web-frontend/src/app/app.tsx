ec4e035a539385e91594b7a04d64d5803191a16b
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-01-13 20:08:16 +0200

    make navbar its own component and fix translation hiccup

--- web-frontend/src/app/app.tsx
+++ web-frontend/src/app/app.tsx
@@ -1,57 +1,14 @@
-import { NavLink, Outlet } from 'react-router-dom';
-
+import { Outlet } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { Navbar } from './design/navbar';
 
 export function App() {
-
+  const { t } = useTranslation(); // don't remove this; used to init i18n
 
   return (
     <div className="m-4">
       <Outlet />
-      {/*<div>*/}
-      {/*  <button onClick={() => i18n.changeLanguage('en')}>EN</button>*/}
-      {/*  <button onClick={() => i18n.changeLanguage('ro')}>RO</button>*/}
-      {/*</div>*/}
-
-      <div className="fixed bottom-2 left-0 w-full px-2">
-        <div className="flex justify-evenly items-center h-[60px] bg-gray-800 rounded-full">
-          <NavLink to="/dashboard">
-            {({ isActive }) => (
-              <span
-                className={`material-icons text-5xl ${
-                  isActive ? 'bg-amber-600 text-white rounded-full p-2' : 'text-white'
-                }`}
-              >
-        send
-      </span>
-            )}
-          </NavLink>
-
-          <NavLink to="/check-in">
-            {({ isActive }) => (
-              <span
-                className={`material-icons text-5xl ${
-                  isActive ? 'bg-amber-600 text-white rounded-full p-2' : 'text-white'
-                }`}
-              >
-        add
-      </span>
-            )}
-          </NavLink>
-
-          <NavLink to="/profile">
-            {({ isActive }) => (
-              <span
-                className={`material-icons text-5xl ${
-                  isActive ? 'bg-amber-600 text-white rounded-full p-2' : 'text-white'
-                }`}
-              >
-        manage_accounts
-      </span>
-            )}
-          </NavLink>
-        </div>
-      </div>
-
+      <Navbar />
     </div>
   );
 }
