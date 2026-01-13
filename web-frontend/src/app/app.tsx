0aa65052bd0a8e2d8c815cbbbe4c10e7853ec066
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-01-13 19:48:07 +0200

    add navigation and profile page

--- web-frontend/src/app/app.tsx
+++ web-frontend/src/app/app.tsx
@@ -1,35 +1,54 @@
-// Uncomment this line to use CSS modules
-import { Outlet, useNavigate } from 'react-router-dom';
-import { userStore } from './store/user.store';
-import { auth } from '../init-firebase-auth';
-import { Trans, useTranslation } from 'react-i18next';
+import { NavLink, Outlet } from 'react-router-dom';
+
 
 export function App() {
-  const setUser = userStore(state => state.setUser);
-  const navigate = useNavigate();
-  const { i18n } = useTranslation();
 
-  const logout = async () => {
-    await auth.signOut();
-    setUser(undefined);
-    navigate('/auth/login', { replace: true });
-  };
 
   return (
-    <div className="h-svh flex justify-center items-center">
+    <div className="m-4">
       <Outlet />
       {/*<div>*/}
       {/*  <button onClick={() => i18n.changeLanguage('en')}>EN</button>*/}
       {/*  <button onClick={() => i18n.changeLanguage('ro')}>RO</button>*/}
       {/*</div>*/}
-      <p><Trans i18nKey="dashboard.nothingYet">Noting to show yet</Trans></p>
-      <button onClick={logout}>Log out</button>
 
       <div className="fixed bottom-2 left-0 w-full px-2">
         <div className="flex justify-evenly items-center h-[60px] bg-gray-800 rounded-full">
-          <span className="material-icons text-5xl text-white">send</span>
-          <span className="material-icons text-5xl bg-amber-600 text-white rounded-full p-2">add</span>
-          <span className="material-icons text-5xl text-white">manage_accounts</span>
+          <NavLink to="/dashboard">
+            {({ isActive }) => (
+              <span
+                className={`material-icons text-5xl ${
+                  isActive ? 'bg-amber-600 text-white rounded-full p-2' : 'text-white'
+                }`}
+              >
+        send
+      </span>
+            )}
+          </NavLink>
+
+          <NavLink to="/check-in">
+            {({ isActive }) => (
+              <span
+                className={`material-icons text-5xl ${
+                  isActive ? 'bg-amber-600 text-white rounded-full p-2' : 'text-white'
+                }`}
+              >
+        add
+      </span>
+            )}
+          </NavLink>
+
+          <NavLink to="/profile">
+            {({ isActive }) => (
+              <span
+                className={`material-icons text-5xl ${
+                  isActive ? 'bg-amber-600 text-white rounded-full p-2' : 'text-white'
+                }`}
+              >
+        manage_accounts
+      </span>
+            )}
+          </NavLink>
         </div>
       </div>
 
