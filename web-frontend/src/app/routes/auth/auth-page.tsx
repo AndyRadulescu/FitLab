f2446deb503679e62507f566f07f76d66561733e
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-01-31 15:42:35 +0200

    make auth page fit desktop nicely

--- web-frontend/src/app/routes/auth/auth-page.tsx
+++ web-frontend/src/app/routes/auth/auth-page.tsx
@@ -27,17 +27,20 @@ export function AuthPage() {
   };
 
   return (
-    <div className="w-full h-svh flex flex-col bg-primary dark:bg-gray-800">
-      <div className="flex-1 flex justify-center items-start pt-12 px-4 mb-4">
+    <div className="w-full min-h-svh flex flex-col lg:flex-row align-center bg-primary dark:bg-gray-800 lg:dark:bg-gray-700 lg:bg-white">
+      <div className="flex-1 flex justify-center items-center p-8 lg:p-12 dark:bg-gray-800 lg:bg-primary lg:rounded-t-none lg:rounded-r-4xl">
         <img
           src="/images/logo-title.svg"
           alt="Logo"
-          className="max-h-64 object-contain"
+          className="max-h-64 lg:max-h-[80%] object-contain"
         />
       </div>
-      <div className="w-full shadow-sm bg-white dark:bg-gray-700 rounded-t-4xl flex justify-center rounded">
-        <div className="w-full py-16 px-[40px]">
+
+      <div
+        className="w-full md:w-[80%] md:ml-[10%] lg:ml-0 lg:max-w-xl bg-white dark:bg-gray-700 rounded-t-4xl lg:rounded-none flex flex-col justify-center">
+        <div className="w-full py-16 px-6 md:px-12 lg:px-16">
           <Outlet />
+
           <div className="flex items-center gap-4 w-full my-4">
             <div className="flex-1 h-px bg-gray-300" />
             <span className="text-sm text-gray-500 dark:text-gray-100 whitespace-nowrap">
@@ -45,22 +48,23 @@ export function AuthPage() {
             </span>
             <div className="flex-1 h-px bg-gray-300" />
           </div>
+
           <div className="w-full">
             <div onClick={onSignInWithGoogle} className="mb-2">
               <SocialButton socialType="google"></SocialButton>
             </div>
-
             <div onClick={onSignInWithFacebook} className="mb-4">
               <SocialButton socialType="facebook"></SocialButton>
             </div>
-            <div className="flex justify-center w-full">
+
+            <div className="flex justify-center w-full mt-6">
               <select
-                className="dark:text-gray-100"
+                className="dark:text-gray-100 bg-transparent border rounded p-1"
                 value={i18n.language}
                 onChange={(e) => i18n.changeLanguage(e.target.value)}
               >
-                <option value="en">🇺🇸English</option>
-                <option value="ro">🇷🇴Română</option>
+                <option value="en">🇺🇸 English</option>
+                <option value="ro">🇷🇴 Română</option>
               </select>
             </div>
           </div>
