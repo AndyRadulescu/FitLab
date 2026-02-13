eab6d2bf78a6e280e7b88b3787fe1b084cbb0d5c
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-02-13 10:34:34 +0200

    hide danger zone

--- web-frontend/src/app/components/danger-zone/danger-zone.tsx
+++ web-frontend/src/app/components/danger-zone/danger-zone.tsx
@@ -1,23 +1,30 @@
-import { Card } from '../design/card';
 import { Trans } from 'react-i18next';
 import { Button } from '../design/button';
 import { SectionHeader } from '../section-header';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { useState } from 'react';
+import { Card } from '../design/card';
 
 export function DangerZone() {
+  const [isShown, setShown] = useState<boolean>(false);
+
   return (
     <>
-      <SectionHeader><Trans i18nKey="profile.danger"/></SectionHeader>
-      <Card isDanger={true}>
+      <div className="cursor-pointer" onClick={() => setShown(!isShown)}>
+        <SectionHeader><Trans i18nKey="profile.danger" /> {isShown ? <ChevronUp /> : <ChevronDown />}</SectionHeader>
+      </div>
+      {isShown && <Card isDanger={true}>
         <div className="flex flex-col md:flex-row gap-4 aling-center justify-center w-full">
           <div className="flex-2">
-            <h3 className="text-lg"><Trans i18nKey="profile.deleteAccount"/></h3>
+            <h3 className="text-lg"><Trans i18nKey="profile.deleteAccount" /></h3>
             <p className="text-md"><Trans i18nKey="profile.delete.info">tst</Trans></p>
           </div>
           <div className="flex-1 flex justify-center items-center">
-            <Button type="danger"><Trans i18nKey="profile.deleteAccount"/></Button>
+            <Button type="danger"><Trans i18nKey="profile.deleteAccount" /></Button>
           </div>
         </div>
       </Card>
+      }
     </>
   );
 }
