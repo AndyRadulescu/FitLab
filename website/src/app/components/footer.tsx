9ad80728c7a08a0bcb7abbdb921c56e24c661594
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-02-17 19:34:43 +0200

    router link active on footer elements

--- website/src/app/components/footer.tsx
+++ website/src/app/components/footer.tsx
@@ -1,6 +1,18 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 export default function Footer() {
+  const pathname = usePathname();
+  console.log(pathname);
+  const navLinks = [
+    { href: '/privacy-policy/', label: 'Privacy Policy' },
+    { href: '/terms/', label: 'Terms of use' },
+    { href: '/data-deletion/', label: 'Data Deletion' },
+  ];
+
   return (
     <footer className="px-6 py-12 border-t border-zinc-900 text-gray-600">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-8">
@@ -8,12 +20,21 @@ export default function Footer() {
           &copy; 2026 AMAZONIA FITLAB. SCIENCE-BACKED FITNESS.
         </div>
         <div className="flex gap-8 text-sm font-medium">
-          <Link href="/privacy-policy" className="hover:text-white">Privacy Policy</Link>
-          <Link href="/terms" className="hover:text-white">Terms of use</Link>
-          <Link href="/data-deletion" className="hover:text-white">Data Deletion</Link>
+          {navLinks.map(({ href, label }) => (
+            <Link
+              key={href}
+              href={href}
+              className={clsx(
+                'hover:text-white',
+                pathname === href && 'text-primary'
+              )}
+            >
+              {label}
+            </Link>
+          ))}
           <a href="mailto:andyradulescu@synapselabs.org" className="hover:text-white">Contact</a>
         </div>
       </div>
     </footer>
-  )
+  );
 }
