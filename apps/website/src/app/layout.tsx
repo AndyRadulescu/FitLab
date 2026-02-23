03125573ff3902bf09eec6e0c2310fc66d6605a5
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-02-23 21:21:14 +0200

    add og-image

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -3,7 +3,20 @@ import Footer from './components/footer';
 
 export const metadata = {
   title: 'Amazonia - FitLab',
-  description: 'Amazonia - FitLab isn\'t just an app; it\'s a digital laboratory for your body. Quantify your mood, track every centimeter of progress, and master the metrics that matter.'
+  description: 'Amazonia - FitLab isn\'t just an app; it\'s a digital laboratory for your body. Quantify your mood, track every centimeter of progress, and master the metrics that matter.',
+  openGraph: {
+    title: 'Amazonia - FitLab',
+    description: 'Amazonia - FitLab isn\'t just an app; it\'s a digital laboratory for your body. Quantify your mood, track every centimeter of progress, and master the metrics that matter.',
+    images: [
+      {
+        url: '/screenshot-desktop-wide.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Amazonia - FitLab Dashboard',
+      },
+    ],
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }: {
