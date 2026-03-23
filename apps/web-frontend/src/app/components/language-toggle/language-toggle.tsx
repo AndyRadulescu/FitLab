c5a5bf526952e749ea7e99ed36361e69decc2da0
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-03-23 18:38:39 +0200

    improve input layout

--- apps/web-frontend/src/app/components/language-toggle/language-toggle.tsx
+++ apps/web-frontend/src/app/components/language-toggle/language-toggle.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Languages } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
+import { Globe, Languages } from 'lucide-react';
+import { Trans, useTranslation } from 'react-i18next';
 import { analytics } from '../../../init-firebase-auth';
 import { logEvent } from 'firebase/analytics';
+import { Card } from '@my-org/shared-ui';
 
 export function LanguageToggle() {
   const { i18n } = useTranslation();
@@ -14,24 +15,40 @@ export function LanguageToggle() {
     localStorage.setItem('language', switcher);
     if (analytics) {
       logEvent(analytics, 'language-switch', {
-        language: switcher,
+        language: switcher
       });
     }
   };
 
   return (
-    <button
-      onClick={toggleLanguage}
-      className="flex items-center gap-2 px-4 py-2 border-gray-400 dark:border-gray-700 dark:bg-slate-400 border-solid border-1 rounded-full shadow-sm transition-colors text-sm font-medium"
-    >
-      <Languages
-        size={16}
-        className="text-primary"
-      />
-      <span className="dark:text-gray-800">{i18n.language.startsWith('en') ? 'English' : 'Română'}</span>
-      <span className="text-xs text-primary border-l pl-2 ml-1">
+    <Card>
+      <div className="flex justify-start items-center w-full mb-4 px-4">
+        <div className="text-center p-2 rounded-md bg-gray-700 dark:bg-black mr-4">
+          <Globe size={24} className="text-gray-200 primary-text-dark" />
+        </div>
+        <div>
+          <h3 className="text-lg"><Trans i18nKey="profile.change.language" /></h3>
+          <p className="tracking-widest text-sm text-gray-500">LOCALIZATION</p>
+        </div>
+      </div>
+      <div className="flex justify-center w-full mb-4">
+        <div className="text-center">
+          <p className="mb-2"><Trans i18nKey="profile.change.language">Change language</Trans></p>
+          <button
+            onClick={toggleLanguage}
+            className="flex items-center gap-2 px-4 py-2 border-gray-400 dark:border-gray-700 dark:bg-slate-400 border-solid border-1 rounded-full shadow-sm transition-colors text-sm font-medium"
+          >
+            <Languages
+              size={16}
+              className="text-primary"
+            />
+            <span className="dark:text-gray-800">{i18n.language.startsWith('en') ? 'English' : 'Română'}</span>
+            <span className="text-xs text-primary border-l pl-2 ml-1">
         {i18n.language.startsWith('en') ? 'RO' : 'EN'}
       </span>
-    </button>
+          </button>
+        </div>
+      </div>
+    </Card>
   );
 }
--- apps/web-frontend/src/app/routes/profile/profile-page.tsx
+++ apps/web-frontend/src/app/routes/profile/profile-page.tsx
@@ -23,24 +23,27 @@ export function ProfilePage() {
     <div>
       <SectionHeader>
         <div>
-          <Trans i18nKey="profile.settings">Profile settings</Trans>
+          <h1 className="text-gray-900 primary-text-dark bold text-2xl md:text-4xl">
+            <Trans i18nKey="profile.settings">Profile settings</Trans>
+          </h1>
           <p
-            className="text-xs md:text-xl text-ellipsis">{auth.currentUser?.displayName ?? auth.currentUser?.email}</p>
+            className="text-xs md:text-xl text-ellipsis text-gray-500">@{auth.currentUser?.displayName ?? auth.currentUser?.email}</p>
         </div>
       </SectionHeader>
-      <Card className="mb-4">
-        <div className="flex justify-center w-full mb-4">
-          <div className="text-center">
-            <p className="mb-2"><Trans i18nKey="profile.change.language">Change language</Trans></p>
-            <LanguageToggle />
-          </div>
-        </div>
+      <div className="mb-4">
+        <LanguageToggle />
+      </div>
+
+      <div className="mb-4">
         <button type="button"
                 onClick={logout}
-                className="w-full bg-gray-200 dark:bg-gray-600 box-border border border-transparent font-medium leading-5 rounded-full text-sm px-4 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#3b5998]/55 mb-4">
+                className="w-full bg-gray-200 dark:bg-gray-600 box-border border border-transparent font-medium leading-5 rounded-full text-sm px-4 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#3b5998]/55">
           <LogOutIcon className="mr-2" />
           <Trans i18nKey="auth.signout">Log out</Trans>
         </button>
+      </div>
+
+      <Card className="mb-4">
         <div className="text-center">
           <a href="https://amazonia-fitlab.ro/privacy-policy/"
              target="_blank"
