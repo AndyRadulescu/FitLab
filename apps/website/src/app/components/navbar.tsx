82e6c343a37bb9e5eb5e3ebd99a8ca3b92552821
Andy Radylescu <radulescu.eduard.andrei@gmail.com>
2026-09-04 15:48:48 +0300

    remove comment

--- apps/website/src/app/components/navbar.tsx
+++ apps/website/src/app/components/navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav className="relative z-30 flex items-center justify-between px-6 py-6 max-w-7xl mx-auto">
-      // TODO: implement nav
+      {/*// TODO: implement nav*/}
     </nav>
   );
 }
